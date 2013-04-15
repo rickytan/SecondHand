963cb4fc7cb08a8bf6093af4c09b46e48f01659c
rickytan <ricky.tan.xin@gmail.com>
2013-04-15 20:44:34 +0800

    add my fav

---
+++ SecondHand/SHMyFavViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHMyFavViewController.h
+//  SecondHand
+//
+//  Created by ricky on 13-4-15.
+//  Copyright (c) 2013年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHMyFavViewController : UITableViewController
+
+@end
