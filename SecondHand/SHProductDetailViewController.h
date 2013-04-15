045f17bb426dfaad176d247e1aaaa2fea163f16a
rickytan <ricky.tan.xin@gmail.com>
2013-04-15 11:15:24 +0800

    new pin. add detail controller. map callout view

---
+++ SecondHand/SHProductDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHProductDetailViewController.h
+//  SecondHand
+//
+//  Created by ricky on 13-4-15.
+//  Copyright (c) 2013年 ricky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHProductDetailViewController : UITableViewController
+
+@end
